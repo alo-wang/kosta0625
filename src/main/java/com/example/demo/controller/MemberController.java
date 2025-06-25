ab058db4c08c1e7dcee82d8f92091da3b4cabfe2
alo-wang <alo201011@gmail.com>
2025-06-25 09:30:23 +0900

    [20250625] update member controller

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 	@GetMapping("/")
 	public String list() {
-		return "customer list";
+		return "customer list11";
 	}
 }
